4d262c565f9ee6a07ba693c7bd6cf141d74d5672
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-17 18:26:03 +0300

    show a procedural code

---
+++ src/main/java/org/example/ProceduralProgramming.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static java.lang.System.*;
+
+public class ProceduralProgramming {
+    public static void main(String[] args) {
+        int baseSalary = 50_000;
+        int extraHours = 10;
+        int hourlyRate = 20;
+
+        int wage = calculateWage(baseSalary, extraHours, hourlyRate);
+        out.println(wage);
+    }
+
+    public static int calculateWage(
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ) {
+        return baseSalary + (extraHours * hourlyRate);
+    }
+}
