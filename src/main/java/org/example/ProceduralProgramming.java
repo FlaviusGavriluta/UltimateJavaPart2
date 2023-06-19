40e20ccbea2ff4f218b33ee7f22f1ade9d104da1
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-19 23:49:15 +0300

    set getters and setters methods

--- src/main/java/org/example/ProceduralProgramming.java
+++ src/main/java/org/example/ProceduralProgramming.java
@@ -5,8 +5,8 @@ import static java.lang.System.*;
 public class ProceduralProgramming {
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.baseSalary = 50_000;
-        employee.hourlyRate = 20;
+        employee.setBaseSalary(50_000);
+        employee.setHourlyRate(20);
         int wage = employee.calculateWage(10);
         out.println(wage);
     }
