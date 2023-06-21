e7711b91298327ea7b052fefca022e396123ea58
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-22 01:52:56 +0300

    static members

--- src/main/java/org/example/ProceduralProgramming.java
+++ src/main/java/org/example/ProceduralProgramming.java
@@ -4,9 +4,10 @@ import static java.lang.System.*;
 
 public class ProceduralProgramming {
     public static void main(String[] args) {
-        new Employee(70_000);
+//        new Employee(70_000);
         var employee = new Employee(
-                50_000,20);
+                50_000, 20);
+        Employee.printNumberOfEmployees();
         int wage = employee.calculateWage();
         out.println(wage);
         var browser = new Browser();
