171c1c6b1494c2522112d6973c0493b4ad70dd38
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-24 16:34:45 +0300

    extracting mortgage calculator class

--- src/main/java/org/example/Section3Refactoring/Finish/MortgageCalculator/MortgageReport.java
+++ src/main/java/org/example/Section3Refactoring/Finish/MortgageCalculator/MortgageReport.java
@@ -5,8 +5,12 @@ import java.text.NumberFormat;
 import static java.lang.System.*;
 
 public class MortgageReport {
+
+    private static MortgageCalculator calculator;
+
     public static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = Main.calculateMortgage(principal, annualInterest, years);
+        calculator = new MortgageCalculator(principal, annualInterest, years);
+        double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         out.println();
         out.println("MORTGAGE");
@@ -19,7 +23,7 @@ public class MortgageReport {
         out.println("PAYMENT SCHEDULE");
         out.println("----------------");
         for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            double balance = Main.calculateBalance(principal, annualInterest, years, month);
+            double balance = calculator.calculateBalance(month);
             out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
     }
