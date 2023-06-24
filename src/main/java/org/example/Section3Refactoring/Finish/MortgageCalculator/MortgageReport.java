f4e2e8f1f1c1e85f8577ad08716b852de3179043
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-24 16:50:46 +0300

    moving away from static members

--- src/main/java/org/example/Section3Refactoring/Finish/MortgageCalculator/MortgageReport.java
+++ src/main/java/org/example/Section3Refactoring/Finish/MortgageCalculator/MortgageReport.java
@@ -6,25 +6,28 @@ import static java.lang.System.*;
 
 public class MortgageReport {
 
-    private static MortgageCalculator calculator;
+    private MortgageCalculator calculator;
 
-    public static void printMortgage(int principal, float annualInterest, byte years) {
-        calculator = new MortgageCalculator(principal, annualInterest, years);
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        out.println();
+        out.println("PAYMENT SCHEDULE");
+        out.println("----------------");
+        for (short month = 1; month <= calculator.getYears() * Main.MONTHS_IN_YEAR; month++) {
+            double balance = calculator.calculateBalance(month);
+            out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
         out.println();
         out.println("MORTGAGE");
         out.println("--------");
         out.println("Monthly Payments: " + mortgageFormatted);
-    }
-
-    public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
-        out.println();
-        out.println("PAYMENT SCHEDULE");
-        out.println("----------------");
-        for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            double balance = calculator.calculateBalance(month);
-            out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
     }
 }
