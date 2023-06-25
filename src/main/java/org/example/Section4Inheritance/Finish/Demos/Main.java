46c7bf369eeff526ae9aa964d17561d73d4644b2
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-25 10:42:23 +0300

    about comparing objects

--- src/main/java/org/example/Section4Inheritance/Finish/Demos/Main.java
+++ src/main/java/org/example/Section4Inheritance/Finish/Demos/Main.java
@@ -5,16 +5,9 @@ import static java.lang.System.*;
 public class Main {
 
     public static void main(String[] args) {
-        var control = new UIControl(true);
-        var textBox = new TextBox();
-        show(control);
-    }
-
-    public static void show(UIControl control) {
-        if (control instanceof TextBox) {
-            var textBox = (TextBox) control;
-            textBox.setText("Hello World");
-        }
-        out.println(control);
+        var point1 = new Point(1, 2);
+        var point2 = new Point(1, 2);
+        out.println(point1.hashCode());
+        out.println(point2.hashCode());
     }
 }
