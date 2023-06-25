dec58cd293442d0b6af8af3363e013a96b288f0e
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-25 09:45:31 +0300

    about the object class

--- src/main/java/org/example/Section4Inheritance/Finish/Demos/Main.java
+++ src/main/java/org/example/Section4Inheritance/Finish/Demos/Main.java
@@ -5,11 +5,17 @@ import static java.lang.System.*;
 public class Main {
 
     public static void main(String[] args) {
-        var control = new UIControl();
-        var textBox = new TextBox();
-        textBox.setText("Hello World");
+        var control = new TextBox();
         control.disable();
+        var obj = new Object();
+        var box1 = new TextBox();
+        var box2 = box1;
+
         out.println(control.isEnabled());
-        out.println(textBox.isEnabled());
+        out.println(obj.toString());
+        out.println(box1.toString());
+        out.println(box1.hashCode());
+        out.println(box2.hashCode());
+        out.println(box1.equals(box2));
     }
 }
