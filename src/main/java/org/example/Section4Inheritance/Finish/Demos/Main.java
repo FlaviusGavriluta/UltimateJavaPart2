5dc62d5125c4b36f605441a26631386c3a4a0e56
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-26 15:48:53 +0300

    method injection

--- src/main/java/org/example/Section4Inheritance/Finish/Demos/Main.java
+++ src/main/java/org/example/Section4Inheritance/Finish/Demos/Main.java
@@ -3,12 +3,10 @@ package org.example.Section4Inheritance.Finish.Demos;
 import static java.lang.System.*;
 
 public class Main {
-
     public static void main(String[] args) {
         var calculator = new TaxCalculator2018(100_000);
-        var report = new TaxReport(calculator);
-        report.show();
-        report.setCalculator(new TaxCalculator2019());
-        report.show();
+        var report = new TaxReport();
+        report.show(calculator);
+        report.show(new TaxCalculator2019());
     }
 }
--- src/main/java/org/example/Section4Inheritance/Finish/Demos/TaxReport.java
+++ src/main/java/org/example/Section4Inheritance/Finish/Demos/TaxReport.java
@@ -5,16 +5,16 @@ import static java.lang.System.*;
 public class TaxReport {
     private TaxCalculator calculator;
 
-    public TaxReport(TaxCalculator calculator) {
-        this.calculator = calculator;
-    }
+//    public TaxReport(TaxCalculator calculator) {
+//        this.calculator = calculator;
+//    }
 
-    public void show() {
+    public void show(TaxCalculator calculator) {
         var tax = calculator.calculateTax();
         out.println(tax);
     }
 
-    public void setCalculator(TaxCalculator calculator) {
-        this.calculator = calculator;
-    }
+//    public void setCalculator(TaxCalculator calculator) {
+//        this.calculator = calculator;
+//    }
 }
