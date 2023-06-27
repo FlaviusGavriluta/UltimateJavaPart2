65bf8f6f838a367eb8fc4633d09e8165acefc2fe
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-06-27 15:57:01 +0300

    adding 3 interfaces files

---
+++ src/main/java/org/example/Section5Interfaces/Start/MyTube/src/com/mytube/VideoDatabase.java
@@ -0,0 +1,5 @@
+package Start.MyTube.src.com.mytube;
+
+public interface VideoDatabase {
+    void store(Video video);
+}
---
+++ src/main/java/org/example/Section5Interfaces/Start/MyTube/src/com/mytube/VideoEncoder.java
@@ -0,0 +1,5 @@
+package Start.MyTube.src.com.mytube;
+
+public interface VideoEncoder {
+    void encode(Video video);
+}
